2f0a54add26e7ee014063f0a668ba4b932e67bee
Olga Bouin <olga.bouin@adeo.com>
2023-05-17 16:14:55 +0200

    feat: round calculated promo price to 2 digits

--- src/core/service/ProductService.py
+++ src/core/service/ProductService.py
@@ -9,18 +9,28 @@ def get_all_products(db: Session):
     results = db.execute(stmt).all()
     return results
 
+
+def get_product(product_id: int, db: Session):
+    stmt = text("SELECT * FROM products WHERE product_id = :product_id LIMIT  1")
+    results = db.execute(stmt, {"product_id": product_id}).all()
+    return results
+
+
 def get_price(product_id: int, db: Session):
     stmt = text("SELECT prix FROM products WHERE product_id = :product_id")
     results = db.execute(stmt, {"product_id": product_id}).mappings().all()
     return results[0].prix
 
+
 def update_promotion(promotion_pourcentage: float, product_id: int, db: Session):
     prix = get_price(product_id, db)
-    promotion_prix = prix*(1 - promotion_pourcentage / 100)
+    promotion_prix = prix * (1 - promotion_pourcentage / 100)
+    promotion_prix_arrondi = round(promotion_prix, 2)
     stmt = text("UPDATE products SET promotion = :promotion WHERE product_id = :product_id")
-    db.execute(stmt, {"promotion": promotion_prix, "product_id": product_id})
+    db.execute(stmt, {"promotion": promotion_prix_arrondi, "product_id": product_id})
     db.commit()
-    return promotion_prix
+    return promotion_prix_arrondi
+
 
 def compute_promotion(product_id: int, db: Session):
     active_promotions = get_active_promotions(product_id, db)
