1501df9d424eccd99a5c838956017d73c78874b3
Olga Bouin <olga.bouin@adeo.com>
2023-05-22 13:55:06 +0200

    test: add api routes testing

--- src/core/models/Product.py
+++ src/core/models/Product.py
@@ -7,6 +7,6 @@ class Product:
     libelle = Column(String)
     description = Column(String)
     prix = Column(Numeric)
-    promotion = Column(Numeric) #TODO : nullable=True is not working, if no data in database, error
-    image = Column(String, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAW4xy0lwmiEo6hcL6Ax3IX1KCQ78A70bArUo3Bsz_gUIi35VZkJEOvUMtMA&s")
+    promotion = Column(Numeric)
+    image = Column(String)
     catalog_id = Column(Integer)
