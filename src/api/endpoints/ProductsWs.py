02f08fc4a72420357ee1867c256890254ffb9468
Olga Bouin <olga.bouin@adeo.com>
2023-05-22 19:28:29 +0200

    feat: add product creation

--- src/api/endpoints/ProductsWs.py
+++ src/api/endpoints/ProductsWs.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 
+from core.schemas.Product import ProductCreate
 from src.core.database.database import connect_with_connector
 from src.core.schemas.Product import Product, Promo
 from src.core.service import ProductService
@@ -12,6 +13,20 @@ routeur = APIRouter()
 @routeur.get("/", response_model=List[Product])
 async def get_all_products(db: Session = Depends(connect_with_connector)):
     return ProductService.get_all_products(db)
+
+
+@routeur.post("/")
+async def create_product(product: ProductCreate, db: Session = Depends(connect_with_connector)):
+    product_id = ProductService.get_max_id(db) + 1
+    complete_product = {"product_id": product_id, "libelle": product.libelle, "description": product.libelle,
+                        "prix": product.prix, "promotion": product.prix, "image": product.image,
+                        "catalog_id": product.catalog_id}
+    return ProductService.create_product(complete_product, db)
+
+
+@routeur.get("/get_max_id/", response_model=int)
+async def get_max_id(db: Session = Depends(connect_with_connector)):
+    return ProductService.get_max_id(db)
 
 
 @routeur.get("/price/{product_id}", response_model=float)
